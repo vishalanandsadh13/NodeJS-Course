44f65bf200192a1d57d4181347ea7778a96070ed
vishalanandsadh13 <vishalanandsadh@gmail.com>
2025-11-20 23:22:13 +0530

    event priority

---
+++ event-priority/app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("Request received");
+
+  process.nextTick(() => console.log("Next Tick"));
+  Promise.resolve().then(() => console.log("Promise"));
+
+  setTimeout(() => console.log("Timeout"), 0);
+  setImmediate(() => console.log("Immediate"));
+
+  res.end("Hello World");
+});
+
+server.listen(3000, () => console.log("Server running"));

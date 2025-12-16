67215a9068c4597a07fb2d63a68ef0f110895002
vishalanandsadh13 <vishalanandsadh@gmail.com>
2025-12-17 02:02:04 +0530

    npm and tools

---
+++ npm and tools - 7/app.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req,res) => {
+  console.log("req",req);
+})
+
+const port = 3000; bn 
+server.listen(port, ()=>{
+  console.log(`Server is listening on port ${port}`);
+})

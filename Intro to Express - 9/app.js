50f6e375d4f70cdd4bfeb2847449340b8d5e3fe2
vishalanandsadh13 <vishalanandsadh@gmail.com>
2025-12-24 15:07:06 +0530

    intro express

---
+++ Intro to Express - 9/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const requestHandler = require("./user");
+const app = express();
+
+app.use((req,res,next) => {
+  console.log("came in firsst middleware", req.url, req.method);
+  next();
+});
+
+app.use((req,res,next) => {
+  console.log("came in second middleware", req.url, req.method);
+ res.send("<p>Response from express server</p>");
+});
+
+const port = 3001;
+app.listen(port, ()=>{
+  console.log(`Server is listening on port ${port}`);
+})

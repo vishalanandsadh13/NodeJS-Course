1751127a87423c555b2118437567c0e99b80eee1
vishalanandsadh13 <vishalanandsadh@gmail.com>
2026-01-17 23:31:56 +0530

    express contact ud

--- Intro to Express - 9/app.js
+++ Intro to Express - 9/app.js
@@ -2,15 +2,16 @@ const express = require("express");
 const requestHandler = require("./user");
 const app = express();
 
-app.use((req,res,next) => {
+app.use("/",(req,res,next) => {
   console.log("came in firsst middleware", req.url, req.method);
+  res.send("<h1>Hello from Express.js server!</h1>");
   next();
-});
+}); 
 
-app.use((req,res,next) => {
+app.post("/submit-details",(req,res,next) => {
   console.log("came in second middleware", req.url, req.method);
  res.send("<p>Response from express server</p>");
-});
+});  
 
 const port = 3001;
 app.listen(port, ()=>{
---
+++ practise set/chapter 9 - contactus/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+app.use("/",(req,res,next)=>{
+  console.log("came in firsst middleware",req.url,req.method);
+  next();
+})
+
+app.use("/",(req,res,next)=>{
+  console.log("came in second middleware",req.url,req.method);
+  next();
+})
+
+// app.use("/",(req,res,next)=>{
+//   console.log("came in third middleware",req.url,req.method);
+//   res.send("<h1>Hello from Express.js server!</h1>");
+// })
+app.get("/",(req,res,next)=>{
+  console.log("handlong / for get",req.url,req.method);
+  res.send("<h1>welcome to complete coding!</h1>");
+});
+
+app.get("/contact-us",(req,res,next)=>{
+  console.log("handlong /contact-us for get",req.url,req.method);
+  res.send("<h1>please give your details!</h1>");
+});
+
+const PORT = 3001
+app.listen(PORT,()=>{
+  console.log(`Server is listening to port ${PORT}`)
+})

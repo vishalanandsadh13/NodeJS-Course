2d05e28734410e3d8ff6bd4f43c1beca2dd36bd5
vishalanandsadh13 <vishalanandsadh@gmail.com>
2026-01-25 22:24:56 +0530

    creating submit details

--- Intro to Express - 9/user.js
+++ Intro to Express - 9/user.js
@@ -1,11 +1,49 @@
- const userRequestHandler = (req, res) => {   
-  console.log(req.url, req.method, req.headers);
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
-  res.write('</html>');
-  res.end();
-} 
-module.exports = userRequestHandler;  
- 
+const userRequestHandler = (req, res) => {
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Enter your Details</h1>");
+    res.write('<form action="/submit-details" method="POST">');
+    res.write(
+      '<input type="text" name="username" placeholder="Enter Username"/>'
+    );
+    res.write("<br/>");
+    res.write('<label for="male">Male </label>');
+    res.write('<input type="radio" id="male" name="gender" value="male"/>');
+    res.write("<br/>");
+    res.write('<label for="female">Female </label>');
+    res.write('<input type="radio" id="female" name="gender" value="female"/>');
+    res.write("<br/>");
+    res.write('<button type="submit">Submit</button>');
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else if (
+    req.url.toLowerCase() === "/submit-details" &&
+    req.method === "POST"
+  ) {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const fullBody = Buffer.concat(body).toString();
+      const params = new URLSearchParams(fullBody);
+      const bodyObject = Object.fromEntries(params);
+      console.log(bodyObject);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      res.end();
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Page Not Found</h1></body>");
+    res.write("</html>");
+    res.end();  
+  }
+};
+module.exports = userRequestHandler;
--- practise set/chapter 9 - contactus/app.js
+++ practise set/chapter 9 - contactus/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 
+const userRequestHandler = require("./user")
+
 app.use("/",(req,res,next)=>{
-  console.log("came in firsst middleware",req.url,req.method);
+  console.log("First Dummy middleware",req.url,req.method);
   next();
 })
 
 app.use("/",(req,res,next)=>{
+  console.log("Second Dummy middleware",req.url,req.method);
+  next();
+})
+
+app.use("/submit-details",(req,res,next)=>{
   console.log("came in second middleware",req.url,req.method);
+  res.send("<h1>Details Submitted Successfully!</h1>");
   next();
 })
 
@@ -22,7 +30,19 @@ app.get("/",(req,res,next)=>{
 
 app.get("/contact-us",(req,res,next)=>{
   console.log("handlong /contact-us for get",req.url,req.method);
-  res.send("<h1>please give your details!</h1>");
+  res.send(`
+    <h1>please give your details!</h1>
+    <form action="/contact-us" method="POST">
+      <input type="text" name="username" placeholder="Enter your name"/>
+      <input type="email" name="email" placeholder="Enter your email"/>
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+app.post("/contact-us",(req,res,next)=>{
+  console.log("handlong /contact-us for post",req.url,req.method);
+  res.send("<h1>Thanks for submitting your details. We will get back to you soon!</h1>");
 });
 
 const PORT = 3001

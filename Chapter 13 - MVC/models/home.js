0bfa3519ccdc25656c5549bb6aea838c1b47d052
vishalanandsadh13 <vishalanandsadh@gmail.com>
2026-04-02 23:31:23 +0530

    reading data from files

--- Chapter 13 - MVC/models/home.js
+++ Chapter 13 - MVC/models/home.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/pathUtil');
 const registeredHomes = [];
 
 module.exports = class Home{
@@ -12,9 +15,22 @@ module.exports = class Home{
 
   save(){
     registeredHomes.push(this);
+    const homeDataPath = path.join(rootDir, 'data', 'homes.json');
+    fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (err) => {
+      if(err){
+        console.log('Error writing to file', err);
+      }
+    });
   }
-  static fetchAll(){
-    return registeredHomes;
+  static fetchAll() {
+    const homeDataPath = path.join(rootDir, 'data', 'homes.json');
+    try {
+      const data = fs.readFileSync(homeDataPath, 'utf-8');
+      return JSON.parse(data);
+    } catch (err) {
+      // If file doesn't exist or is empty, return empty array
+      return [];
+    }
   }
 
   

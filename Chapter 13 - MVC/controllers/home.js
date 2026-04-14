b57781dece0745773bfd1cd4210e7b336165acb1
vishalanandsadh13 <vishalanandsadh@gmail.com>
2026-04-15 00:55:43 +0530

    added structure in views

--- Chapter 13 - MVC/controllers/home.js
+++ Chapter 13 - MVC/controllers/home.js
@@ -1,19 +1,19 @@
 const Home = require('../models/home');
 
 exports.getAddHome = (req, res, next) => {
-  res.render('addHome', { pageTitle: 'Add Home to airbnb', currentPage: 'Add Home' });
+  res.render('host/addHome', { pageTitle: 'Add Home to airbnb', currentPage: 'Add Home' });
 };
 
 exports.postAddHome = (req, res, next) => {
   const { houseName, location, price, rating, photoUrl } = req.body;
   const home = new Home(houseName, location, price, rating, photoUrl);
   home.save();
-  res.render('homeAdded', { pageTitle: 'Home Added Successfully', currentPage: 'Home Added' });
+  res.render('host/homeAdded', { pageTitle: 'Home Added Successfully', currentPage: 'Home Added' });
 };
 
 exports.getHome = (req, res, next) => {
   const registeredHomes = Home.fetchAll();
-  res.render('home', {
+  res.render('store/home-list', {
     registeredHomes,
     pageTitle: 'airbnb Home',
     currentPage: 'Home'

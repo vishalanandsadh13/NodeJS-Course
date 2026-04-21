07b191f68d4215b55017f5824bc55960a1df40ad
vishalanandsadh13 <vishalanandsadh@gmail.com>
2026-04-22 00:04:15 +0530

    fixed home adding issue

--- Chapter 14 - Dynamic Path/app.js
+++ Chapter 14 - Dynamic Path/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(storeRouter);
 app.use("/host", hostRouter);
 

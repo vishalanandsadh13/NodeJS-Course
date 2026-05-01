69f4bd853f9ece417a777b1be4b7562bcf8388a3
vishalanandsadh13 <vishalanandsadh@gmail.com>
2026-05-01 22:53:49 +0530

    request error fixed

--- event-priority/app.js
+++ event-priority/app.js
@@ -1,7 +1,6 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Request received");
 
   process.nextTick(() => console.log("Next Tick"));
   Promise.resolve().then(() => console.log("Promise"));
